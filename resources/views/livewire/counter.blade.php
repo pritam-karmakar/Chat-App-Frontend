1fd4209701d77ea9434359e940ea8ce78469c0ef
pritam-karmakar <karmakarpritamkarmakarpritam003@gmail.com>
2025-10-21 11:43:14 +0530

    Remove all files from the repository

--- resources/views/livewire/counter.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h2>Counter {{ $count }}</h2>
-    <button class="btn btn-success btn-sm" wire:click="increment">+</button>
-    <button class="btn btn-danger btn-sm" wire:click="decrement">-</button>
-</div>
